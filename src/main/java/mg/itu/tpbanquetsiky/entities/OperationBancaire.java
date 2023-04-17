fbb7251ff6fb8975857252f197be97f7f3de844c
ANDRIANAIVOSOA Tsiky <ANDRIANAIVOSOA Tsiky@DESKTOP-EO5F7E9>
2023-04-17 20:35:52 +0300

    pb d'affichage de date

--- src/main/java/mg/itu/tpbanquetsiky/entities/OperationBancaire.java
+++ src/main/java/mg/itu/tpbanquetsiky/entities/OperationBancaire.java
@@ -5,11 +5,11 @@
 package mg.itu.tpbanquetsiky.entities;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Date;
 
 /**
  *
@@ -18,22 +18,31 @@ import jakarta.persistence.Id;
 @Entity
 public class OperationBancaire implements Serializable {
 
-    private static final long serialVersionUID = 1L; 
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-   
-    private LocalDateTime dateOperation;
-  
+
+    private Date dateOperation;
+
     private int montant;
-                    
-    public OperationBancaire() { }
-                    
+
+    public OperationBancaire() {
+    }
+
     public OperationBancaire(String description, int montant) {
         this.description = description;
         this.montant = montant;
-        dateOperation = LocalDateTime.now();
+        dateOperation = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getDescription() {
@@ -44,11 +53,11 @@ public class OperationBancaire implements Serializable {
         this.description = description;
     }
 
-    public LocalDateTime getDateOperation() {
+    public Date getDateOperation() {
         return dateOperation;
     }
 
-    public void setDateOperation(LocalDateTime dateOperation) {
+    public void setDateOperation(Date dateOperation) {
         this.dateOperation = dateOperation;
     }
 
@@ -59,5 +68,5 @@ public class OperationBancaire implements Serializable {
     public void setMontant(int montant) {
         this.montant = montant;
     }
-    
+
 }
